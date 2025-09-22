af35ee1ebf14df7e46d682eb1a504d2322712ee1
tanchengzi0430 <tanchengzi0430@gmail.com>
2025-09-22 21:10:37 +0800

    seq_emb.sh

--- scripts/Nucleotide_Transformer/seq_emb.sh
+++ scripts/Nucleotide_Transformer/seq_emb.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 
-export reference_sheet="/home/ma_run_ze/lzm/rnagym/fitness/reference_sheet_final.csv"
-export output_dir="/data_share/marunze/lzm/rnagym/fitness/embeddings/nt_v2_50m/"
-export dms_data_dir="/home/ma_run_ze/lzm/rnagym/fitness/fitness_processed_assays"
+export reference_sheet=""
+export output_dir=""
+export dms_data_dir=""
 conda activate genslm
 mkdir -p "$output_dir"
-export HF_ENDPOINT=https://hf-mirror.com
-export CUDA_VISIBLE_DEVICES=7  # Set to the GPU you want to use
 export model_location=export model_location="InstaDeepAI/nucleotide-transformer-v2-50m-multi-species"
-# Get the current index from the array (0-31)
-    # Run the scoring script with the array task ID
     python3 seq_emb.py \
     --output_dir_path "$output_dir" \
     --ref_sheet "$reference_sheet" \
