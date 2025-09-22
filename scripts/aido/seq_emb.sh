7fe1d81adaa2a5523cd83773fedfe412f160cf3e
tanchengzi0430 <tanchengzi0430@gmail.com>
2025-09-22 21:37:17 +0800

    seq_emb.sh

--- scripts/aido/seq_emb.sh
+++ scripts/aido/seq_emb.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# script used to score Nucleotide Transformer model
-export reference_sheet="/home/ma_run_ze/lzm/rnagym/fitness/reference_sheet_final.csv"
-export dms_data_dir="/home/ma_run_ze/lzm/rnagym/fitness/fitness_processed_assays"
+
+export reference_sheet=""
+export dms_data_dir=""
 mkdir -p "$output_scores_dir"
-# https://huggingface.co/InstaDeepAI/nucleotide-transformer-2.5b-multi-species
-DMS_index=$((44))  # Change this to the desired DMS index
-export HF_ENDPOINT=https://hf-mirror.com
+DMS_index=$((44)) 
 source /data4/marunze/aido/bin/activate
 
-export CUDA_VISIBLE_DEVICES=1  # Set to the GPU you want to use
-    # Remove existing score files and create a new summary file
-# Get the current index from the array (0-31)
 
-
-
-
-export output_dir="/data_share/marunze/lzm/rnagym/fitness/embeddings/aido/"
-export dms_data_dir="/home/ma_run_ze/lzm/rnagym/fitness/fitness_processed_assays"
+export output_dir=""
+export dms_data_dir=""
 mkdir -p "$output_dir"
-# Get the current index from the array (0-31)
-
-    # Run the scoring script with the array task ID
     python seq_emb.py \
     --output_dir_path "$output_dir" \
     --ref_sheet "$reference_sheet" \
     --dms_dir_path "$dms_data_dir" \
-
