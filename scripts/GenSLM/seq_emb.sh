c3eff850bec1062b967d04687e46f67d9063ccc1
Jiahao Tan <nicolobarella@sjtu.edu.cn>
2025-09-22 20:29:14 +0800

    seq_emb.sh

--- scripts/GenSLM/seq_emb.sh
+++ scripts/GenSLM/seq_emb.sh
@@ -6,10 +6,6 @@ export output_dir=""
 export dms_data_dir=""
 conda activate genslm
 mkdir -p "$output_dir"
-export HF_ENDPOINT=https://hf-mirror.com
-export CUDA_VISIBLE_DEVICES=1  # Set to the GPU you want to use
-# Get the current index from the array (0-31)
-    # Run the scoring script with the array task ID
     python3 seq_emb.py \
     --output_dir_path "$output_dir" \
     --ref_sheet "$reference_sheet" \
