52877d96c80048c4faf506af038446201c3b37ec
Jiahao Tan <nicolobarella@sjtu.edu.cn>
2025-09-22 20:25:13 +0800

    seq_emb.sh

--- scripts/GenSLM/seq_emb.sh
+++ scripts/GenSLM/seq_emb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-export reference_sheet="/home/ma_run_ze/lzm/rnagym/fitness/reference_sheet_final.csv"
-export output_dir="/data_share/marunze/lzm/rnagym/fitness/embeddings/genslm/"
-export dms_data_dir="/home/ma_run_ze/lzm/rnagym/fitness/fitness_processed_assays"
+export reference_sheet=""
+export output_dir=""
+export dms_data_dir=""
 conda activate genslm
 mkdir -p "$output_dir"
 export HF_ENDPOINT=https://hf-mirror.com
@@ -14,4 +14,4 @@ export CUDA_VISIBLE_DEVICES=1  # Set to the GPU you want to use
     --output_dir_path "$output_dir" \
     --ref_sheet "$reference_sheet" \
     --dms_dir_path "$dms_data_dir" \
-    --model_cache_dir "/data4/marunze/GenSLM/"
+    --model_cache_dir ""
