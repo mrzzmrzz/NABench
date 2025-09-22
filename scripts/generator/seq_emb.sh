cacd0d7dceb39b641aed198cdc958a13ac30dda6
tanchengzi0430 <tanchengzi0430@gmail.com>
2025-09-22 21:56:26 +0800

    seq_emb.sh

--- scripts/generator/seq_emb.sh
+++ scripts/generator/seq_emb.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 
-export reference_sheet="/home/ma_run_ze/lzm/rnagym/fitness/reference_sheet_final.csv"
-export output_dir="/data_share/marunze/lzm/rnagym/fitness/embeddings/generator/"
-export dms_data_dir="/home/ma_run_ze/lzm/rnagym/fitness/fitness_processed_assays"
+export reference_sheet=""
+export output_dir=""
+export dms_data_dir=""
 conda activate genslm
 mkdir -p "$output_dir"
-export HF_ENDPOINT=https://hf-mirror.com
-export CUDA_VISIBLE_DEVICES=6  # Set to the GPU you want to use
-# Get the current index from the array (0-31)
-    # Run the scoring script with the array task ID
     python3 seq_emb.py \
     --output_dir_path "$output_dir" \
     --ref_sheet "$reference_sheet" \
